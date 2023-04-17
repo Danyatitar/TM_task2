90bd802f0d6cb07828d8f33d3e079edc80e2dcee
DanyloTytarenko <87567245+Danyatitar@users.noreply.github.com>
2023-04-18 00:22:55 +0300

    finish task

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# TM_task2
+# How to start a project
+
+To start this project you can download a project and open file index.html or just click to the link on section about.
+
+# Js Queue Visual
+
+This is a visual representation of queue.
+
+# Queue
+
+This is a structured data type that works according to the FIFO principle. That is, you can add elements to the queue and delete them, but only the element which has been in the queue for the longest time will be removed. Also, the queue stack is saved through page reloads.
---
+++ main.js
@@ -0,0 +1,98 @@
+const add_btn = document.querySelector(".add-btn");
+const remove_btn = document.querySelector(".remove-btn");
+const input = document.querySelector(".input");
+const box = document.querySelector(".box");
+const err = document.querySelector(".error");
+const clear_btn = document.querySelector(".clear-btn");
+
+const max_length = 19;
+let queue = {
+  items: [],
+  addIndex: 0,
+  removeIndex: 0,
+};
+
+function load() {
+  window.addEventListener("load", () => {
+    const result = localStorage.getItem("items");
+    if (result) {
+      queue.items = result.split(",").map((item) => parseFloat(item));
+      renderQueue(queue);
+      queue.addIndex = queue.items.length;
+    }
+  });
+}
+
+function renderQueue(queue) {
+  queue.items.forEach((item, index) => {
+    addItem(item, queue, index + 1);
+  });
+}
+
+function handleAdd() {
+  add_btn.addEventListener("click", () => {
+    if (!input.value) {
+      err.classList.remove("hidden");
+      err.innerHTML = "Error! Empty input!";
+    } else if (queue.items.length === max_length) {
+      err.classList.remove("hidden");
+      err.innerHTML = "Error! Queue size is 19!";
+    } else {
+      err.classList.add("hidden");
+      queue.items.push(input.value);
+      queue.addIndex++;
+      addItem(input.value, queue, queue.addIndex);
+      localStorage.setItem("items", queue.items);
+      input.value = null;
+    }
+  });
+}
+
+function handleRemove() {
+  remove_btn.addEventListener("click", () => {
+    if (!queue.items.length) {
+      err.classList.remove("hidden");
+      err.innerHTML = "Error! The Queue is already empty!";
+    } else {
+      err.classList.add("hidden");
+      queue.removeIndex++;
+      removeItem(queue);
+      queue.items.shift();
+      localStorage.setItem("items", queue.items);
+    }
+  });
+}
+
+function handleClear() {
+  clear_btn.addEventListener("click", () => {
+    err.classList.add("hidden");
+    if (queue.items !== []) {
+      queue.removeIndex += queue.items.length;
+      queue.items = [];
+      const removedItems = document.querySelectorAll(".box-item");
+      removedItems.forEach((item) => item.remove());
+      localStorage.setItem("items", queue.items);
+    }
+  });
+}
+
+function addItem(value, queue, index) {
+  const queueItem = document.createElement("div");
+  queueItem.classList.add("box-item");
+  queueItem.classList.add(`item-${index}`);
+  queueItem.innerHTML = value;
+  box.append(queueItem);
+}
+
+function removeItem(queue) {
+  document.querySelector(`.item-${queue.removeIndex}`).remove();
+}
+
+const main = () => {
+  load();
+  handleAdd();
+  handleRemove();
+  handleClear();
+};
+
+main();
